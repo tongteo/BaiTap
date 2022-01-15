57ece5986bc28cf1f6b687ddd1ade10aa9ac4e1f
Trung Hua <vinhtrung1412@gmail.com>
2022-01-15 10:25:01 +0700

    Bai Tap C

---
+++ h_dem_so_le.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n; cin>>n;
+	int a[n];
+	for(int i=0; i<n;  i++)
+	{
+		cin>>a[i];
+	}
+	int so_le;
+	for(int i=0; i<n;  i++)
+	{
+		if(a[i]%2!=0)
+		{
+			++so_le;
+		}
+	}cout<<so_le<<endl;
+	for(int i=0; i<n;  i++)
+	{
+		if(a[i]%2!=0)
+		{
+			cout<<a[i]<<" ";
+		}
+	}
+	cout<<endl;
+	return 0;
+}
