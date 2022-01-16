77137a612f0aa08653275683dac4835243acf399
Trung Hua <vinhtrung1412@gmail.com>
2022-01-16 10:48:11 +0700

    Bai 2

---
+++ Bai2/a_mang_tang_dan.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int check(int a[], int n){
+	for(int i=0;i<n-1;i++){
+		if(a[i]>=a[i+1])
+			return 0;
+	}
+	return 1;
+}
+int main(){
+	int n;cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	if(check(a,n))
+		cout<<"YES";
+	else cout <<"NO";
+
+	cout<<endl;
+	return 0;
+}
