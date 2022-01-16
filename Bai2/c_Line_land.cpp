28eece33eae9a37b39b6aca749b9227d8319e995
Trung Hua <vinhtrung1412@gmail.com>
2022-01-16 16:20:37 +0700

    Lineland

---
+++ Bai2/c_Line_land.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int mini(int a, int b){
+	return a<b?a:b;
+}
+int maxi(int a, int b){
+	return a>b?a:b;
+}
+int main(){
+	int n;cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	for(int i=0;i<n;i++){
+		if (i==0) cout<<a[1]-a[0]<<" "<<a[n-1]-a[0]<<endl;//phan tu dau tien trong mang
+		else if(i==n-1) cout<<a[n-1]-a[n-2]<<" "<<a[n-1]-a[0]<<endl;//phan tu cuoi cung trong mang
+		else cout<<mini(a[i]-a[i-1],a[i+1]-a[i])<<" "<<maxi(a[i]-a[0],a[n-1]-a[i])<<endl;//phan tu bat ky
+	}
+	return 0;
+}
