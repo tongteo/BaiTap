2531476efc74d3f389385cfb13421a34d1016bc7
Trung Hua <vinhtrung1412@gmail.com>
2022-01-16 16:23:22 +0700

    Bai 2

--- Bai2/c_Line_land.cpp
+++ Bai2/c_Line_land.cpp
@@ -15,7 +15,7 @@ int main(){
 	for(int i=0;i<n;i++){
 		if (i==0) cout<<a[1]-a[0]<<" "<<a[n-1]-a[0]<<endl;//phan tu dau tien trong mang
 		else if(i==n-1) cout<<a[n-1]-a[n-2]<<" "<<a[n-1]-a[0]<<endl;//phan tu cuoi cung trong mang
-		else cout<<mini(a[i]-a[i-1],a[i+1]-a[i])<<" "<<maxi(a[i]-a[0],a[n-1]-a[i])<<endl;//phan tu bat ky
+		else cout<<mini(a[i]-a[i-1],a[i+1]-a[i])<<" "<<maxi(a[i]-a[0],a[n-1]-a[i])<<endl;//phan tu bat ky trong mang
 	}
 	return 0;
 }
